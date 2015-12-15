0499b7cbb96f86a3ff1acb6a40ebe5fccbc2ad77
Noe Emmanuel Pante <neapante@gmail.com>
2015-12-16 05:02:52 +0800

    Update Script
    Operations are now working.

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,68 +1,118 @@
-  $(document).ready(function() {
+var a = [],
+  parsedValue;
 
-    var sum = 10 + 10;
-    var val1 = [];
-    var val2 = [];
-    var myArray = [];
-    //output the value
-    //$("#result").html(sum);
-    $("#1").click(function() {
-      val1.push(1);
-      $("#result").text(val1);
-      console.log(val1);
-    });
-    $("#2").click(function() {
-      val1.push(2);
-      console.log(val1);
-    });
-    $("#3").click(function() {
-      val1.push(3);
-      console.log(val1);
-    });
-    $("#4").click(function() {
-      val1.push(4);
-      console.log(val1);
-    });
-    $("#5").click(function() {
-      val1.push(5);
-      console.log(val1);
-    });
-    $("#6").click(function() {
-      val1.push(6);
-      console.log(val1);
-    });
-    $("#7").click(function() {
-      val1.push(7);
-      console.log(val1);
-    });
-    $("#8").click(function() {
-      val1.push(8);
-      console.log(val1);
-    });
-    $("#9").click(function() {
-      val1.push(9);
-      console.log(val1);
-    });
-    $("#0").click(function() {
-      val1.push(0);
-      console.log(val1);
-    });
+$(document).ready(function() {
 
-    //operation
-    $("#add").click(function() {
-      //convert the array string to intiger
-      val1 = parseInt(val1.join(''), 10);
-      $("#result").text(add(val1, 2));
-    });
-    
-    
-    //clear val1 content
-    $("#clear").click(function(){
-      val1 = [];
-      $("#result").text("0");
-    });
+  var val = [];
+  //var firstVal = [];
+  //var secondVal = [];
+  //var myArray = [];
+  //output the value
+  //$("#result").html(sum);
+  $("#1").click(function() {
+    storeValue(1);
+  });
+  $("#2").click(function() {
+    storeValue(2);
+  });
+  $("#3").click(function() {
+    storeValue(3);
+  });
+  $("#4").click(function() {
+    storeValue(4);
+  });
+  $("#5").click(function() {
+    storeValue(5);
+  });
+  $("#6").click(function() {
+    storeValue(6);
+  });
+  $("#7").click(function() {
+    storeValue(7);
+  });
+  $("#8").click(function() {
+    storeValue(8);
+  });
+  $("#9").click(function() {
+    storeValue(9);
+  });
+  $("#0").click(function() {
+    storeValue(0);
   });
 
-  function add(var1, var2) {
-    return var1 + var2;
+  //operation
+  $("#add").click(function() {
+    //convert the array string to integer
+    //val1 = parseInt(val1.join(''), 10);
+    val.push(parsedValue);
+    val.push("+");
+    clearValue();
+    console.log(val);
+  });
+
+  $("#subtract").click(function() {
+    //convert the array string to integer
+    //val1 = parseInt(val1.join(''), 10);
+    val.push(parsedValue);
+    val.push("-");
+    clearValue();
+    console.log(val);
+  })
+  $("#multiply").click(function() {
+    //convert the array string to integer
+    //val1 = parseInt(val1.join(''), 10);
+    val.push(parsedValue);
+    val.push("*");
+    clearValue();
+    console.log(val);
+  })
+  $("#divide").click(function() {
+    //convert the array string to integer
+    //val1 = parseInt(val1.join(''), 10);
+    val.push(parsedValue);
+    val.push("/");
+    clearValue();
+    console.log(val);
+  })
+
+  //calculate values
+  $("#equal").click(function() {
+    val.push(parsedValue);
+    console.log(val);
+    $("#result").text(calculate(val));
+  });
+
+  //clear val content
+  $("#clear").click(function() {
+    a = [];
+    parsedValue = 0;
+    val = [];
+    $("#result").text("0");
+  });
+
+  //equals
+});
+
+function calculate(val) {
+  if (val[1] == "+") {
+    return val[0] + val[2];
+  } else if (val[1] == "-") {
+    return val[0] - val[2];
+  } else if (val[1] == "*") {
+    return val[0] * val[2];
+  } else if (val[1] == "/") {
+    return val[0] / val[2];
   }
+}
+
+function storeValue(number) {
+  a.push(number);
+  parsedValue = parseInt(a.join(''), 10);
+  console.log(a);
+  $("#result").text(parsedValue);
+}
+
+function clearValue() {
+  a = [];
+  parsedValue = 0;
+}
