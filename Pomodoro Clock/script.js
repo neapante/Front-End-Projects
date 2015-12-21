1b35a3af57768694c5a6dd2bd14dfbf5972a79f8
Noe Emmanuel Pante <neapante@gmail.com>
2015-12-21 19:32:16 +0800

    Update Pomodoro Clock

--- Pomodoro Clock/script.js
+++ Pomodoro Clock/script.js
@@ -3,7 +3,8 @@ var h1 = document.getElementsByTagName('h1')[0],
     stop = document.getElementById('stop'),
     clear = document.getElementById('clear'),
     seconds = 0, minutes = 0, hours = 0,
-    t;
+    t,
+    sessionLength = 25, breakLength = 5;
 
 var wav = 'http://www.oringz.com/oringz-uploads/sounds-882-solemn.mp3';
 var audio = new Audio(wav);
@@ -11,6 +12,9 @@ var audio = new Audio(wav);
 
 var state = "work";
 function add() {
+    sessionLength--;
+  breakLength--;
+  console.log("this is session Length : " + sessionLength);
     seconds++;
     if (seconds >= 60) {
         seconds = 0;
@@ -24,19 +28,24 @@ function add() {
     h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
     
     console.log (minutes);
-    if (minutes > 24 && state == "work") {
+  console.log (sessionLength);
+  console.log (breakLength);
+    
+    if (minutes > sessionLength && state == "work") {
             h1.textContent = "00:00:00";
             seconds = 0; minutes = 0; hours = 0;
             state = "rest";
             audio.play();
       
-    } else if (minutes > 4 && state == "rest") {
+    } else if (minutes > breakLength && state == "rest") {
             h1.textContent = "00:00:00";
             seconds = 0; minutes = 0; hours = 0;
             state = "work";
             audio.play();
     }
-  
+    
+  sessionLength++;
+  breakLength++;
     timer();
 }
 function timer() {
@@ -59,3 +68,28 @@ clear.onclick = function() {
     h1.textContent = "00:00:00";
     seconds = 0; minutes = 0; hours = 0;
 }
+
+
+  $("#addSL").click(function() {
+    sessionLength = sessionLength + 1;
+    $("#displaySL").text(sessionLength);
+    //console.log(sessionLength);
+  });
+
+  $("#subSL").click(function() {
+    sessionLength = sessionLength - 1;
+    $("#displaySL").text(sessionLength);
+    //console.log(sessionLength);
+  });
+
+  $("#addBL").click(function() {
+    breakLength = breakLength + 1;
+    $("#displayBL").text(breakLength);
+    //console.log(sessionLength);
+  });
+
+  $("#subBL").click(function() {
+    breakLength = breakLength - 1;
+    $("#displayBL").text(breakLength);
+    //console.log(sessionLength);
+  });
